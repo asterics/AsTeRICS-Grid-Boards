a7d8f5107a6303f8cf8847a4fac74a5d196f0ba6
Benjamin Klaus <benjamin@asterics-foundation.org>
2025-02-05 14:57:04 +0100

    generating translation file for predefined actions, https://github.com/asterics/AsTeRICS-Grid/issues/472

--- scripts/generatePredefinedActions.js
+++ scripts/generatePredefinedActions.js
@@ -2,24 +2,62 @@ const fs = require('fs');
 const path = require('path');
 const BASE_FOLDER = "predefined_actions"
 const startPath = path.join(__dirname, "..", BASE_FOLDER);
+const i18nPath = path.join(startPath, "i18n");
 const OUTPUT_FILENAME = process.argv[2] && process.argv[2] === "beta" ? "live_predefined_actions_beta.json" : "live_predefined_actions.json";
+const I18N_FILENAME = "i18n.en.json";
 
 let folderContent = fs.readdirSync(startPath, {withFileTypes: true}).filter(e => !e.isDirectory() && e.name.endsWith(".json"));
+let i18nContent = fs.readFileSync(path.join(i18nPath, I18N_FILENAME), 'utf-8');
+if (i18nContent) {
+    i18nContent = JSON.parse(i18nContent);
+} else {
+    i18nContent = {};
+}
 
 main();
 async function main() {
     let jsonObjects = [];
+    let i18nStrings = [];
     for (let file of folderContent) {
         let fileContent = fs.readFileSync(path.join(file.path, file.name), 'utf-8');
         try {
             let parsed = JSON.parse(fileContent);
             if (!parsed.skip) {
                 jsonObjects.push(parsed);
+                let actions = parsed.actions || [];
+                for (let action of actions) {
+                    i18nStrings.push(action.name);
+                    let customValues = action.customValues || [];
+                    for (let customValue of customValues) {
+                        i18nStrings.push(customValue.name);
+                        if (customValue.type === 'select') {
+                            i18nStrings = i18nStrings.concat(customValue.values);
+                        }
+                    }
+                }
             }
         } catch (e) {
             console.warn("failed to parse JSON of file", file.name, e);
         }
     }
+
+    let existingI18nStrings = Object.keys(i18nContent);
+    for (let existingKey of existingI18nStrings) {
+        if (!i18nStrings.includes(existingKey)) {
+            console.info(`Existing translation "${existingKey}" removed because not existing anymore.`);
+            delete i18nContent[existingKey];
+        }
+    }
+    i18nStrings = i18nStrings.filter(string => !!string && !string.includes(' ') && !existingI18nStrings.includes(string));
+    i18nStrings = new Array(...new Set(i18nStrings));
+    for (let newString of i18nStrings) {
+        i18nContent[newString] = newString;
+    }
+    if (i18nStrings.length > 0) {
+        console.warn(`${i18nStrings.length} new strings were added to '${I18N_FILENAME}' which need translation! New ones: ${JSON.stringify(i18nStrings)}`);
+    }
+
+    fs.writeFileSync(path.join(i18nPath, I18N_FILENAME), JSON.stringify(i18nContent, null, 2));
     fs.writeFileSync(path.join(__dirname, "..", OUTPUT_FILENAME), JSON.stringify(jsonObjects));
     console.log(`Successfully written ${jsonObjects.length} elements to ${OUTPUT_FILENAME}!`)
 }
